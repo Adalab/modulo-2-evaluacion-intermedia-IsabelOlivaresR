c3fdfbf0d949f397c5ad7d8d16d5c7f8f49c16f1
IsabelOlivaresR <isabel.olivares6@gmail.com>
2020-06-30 10:20:40 +0200

    add improved js

--- js/main.js
+++ js/main.js
@@ -4,40 +4,48 @@
 const enterNumber = document.querySelector('.js-enter-number');
 const tryButton = document.querySelector('.js-try-button');
 const hint = document.querySelector('.js-hint');
-const AttempsBox = document.querySelector('.js-attemps');
-const dataNumber = {};
+const attempsBox = document.querySelector('.js-attemps');
+//const dataNumber = {};
 let attemps = 0;
 
 function getRandomNumber(max) {
-  const randomNumber = Math.ceil(Math.random() * max);
-  return randomNumber;
+  return Math.ceil(Math.random() * max);
 }
+
+const randomNumber = getRandomNumber(100);
+
 console.log(randomNumber);
 
-function getEnterNumber() {
+/*function getEnterNumber() {
   dataNumber.number = enterNumber.value;
 }
-console.log(dataNumber.number);
+console.log(dataNumber.number);*/
 
 function handlerCalculation() {
-  getEnterNumber();
-  if (dataNumber.number > randomNumber) {
+  //getEnterNumber();
+  const inputValue = enterNumber.value;
+
+  if (inputValue > randomNumber) {
     hint.innerHTML = 'Demasiado alto.';
     let attemps = attemps + 1;
-  } else if (dataNumber.number < randomNumber) {
+    attempsBox.innerHTML = `Número de intentos: ${attemps}`;
+  } else if (inputValue < randomNumber) {
     hint.innerHTML = 'Demasiado bajo';
     let attemps = attemps + 1;
-  } else if (dataNumber.number === randomNumber) {
+    attempsBox.innerHTML = `Número de intentos: ${attemps}`;
+  } else if (inputValue === randomNumber) {
     hint.innerHTML = 'Has ganado campeona!!!';
-  } else if (dataNumber.number > 100) {
+  } else if (inputValue > 100) {
     ('El número debe estar entre 1 y 100.');
     let attemps = attemps + 1;
-  } else if (dataNumber.number < 1) {
+    attempsBox.innerHTML = `Número de intentos: ${attemps}`;
+  } else if (inputValue < 1) {
     ('El número debe estar entre 1 y 100.');
     let attemps = attemps + 1;
+    attempsBox.innerHTML = `Número de intentos: ${attemps}`;
   }
 }
 
-window.addEventListener('load', getRandomNumber);
-enterNumber.addEventListener('keyup', getEnterNumber);
+//window.addEventListener('load', getRandomNumber);
+//enterNumber.addEventListener('keyup', getEnterNumber);
 tryButton.addEventListener('click', handlerCalculation);
