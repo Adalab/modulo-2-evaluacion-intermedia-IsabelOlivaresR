6b39592d707d242c68eb2d1073188d101a532619
IsabelOlivaresR <isabel.olivares6@gmail.com>
2020-07-07 20:34:58 +0200

    finish exercise and fix some errors

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const hint = document.querySelector('.js-hint');
 const attempsBox = document.querySelector('.js-attemps');
 let attemps = 0;
 
+//Choose random number
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
@@ -14,36 +15,31 @@ function getRandomNumber(max) {
 const randomNumber = getRandomNumber(100);
 console.log(randomNumber);
 
-/*function getEnterNumber() {
-  dataNumber.number = enterNumber.value;
-}
-console.log(dataNumber.number);*/
-
-function handlerCalculation() {
-  //getEnterNumber();
+//Comparing rules
+function compareNumbers() {
   const inputValue = enterNumber.value;
 
-  if (inputValue > randomNumber) {
+  if (inputValue > 100 || inputValue < 1) {
+    hint.innerHTML = 'El número debe estar entre 1 y 100.';
+  } else if (inputValue > randomNumber) {
     hint.innerHTML = 'Demasiado alto.';
-    /*let attemps = attemps + 1;
-    attempsBox.innerHTML = `Número de intentos: ${attemps}`;*/
   } else if (inputValue < randomNumber) {
     hint.innerHTML = 'Demasiado bajo';
-    /*let attemps = attemps + 1;
-    attempsBox.innerHTML = `Número de intentos: ${attemps}`;*/
-  } else if (inputValue == randomNumber) {
+  } else if (parseInt(inputValue) === randomNumber) {
     hint.innerHTML = 'Has ganado campeona!!!';
-  } else if (inputValue > 100) {
-    ('El número debe estar entre 1 y 100.');
-    /*/let attemps = attemps + 1;
-    attempsBox.innerHTML = `Número de intentos: ${attemps}`;*/
-  } else if (inputValue < 1) {
-    ('El número debe estar entre 1 y 100.');
-    /*let attemps = attemps + 1;
-    attempsBox.innerHTML = `Número de intentos: ${attemps}`;*/
   }
 }
 
-//window.addEventListener('load', getRandomNumber);
-//enterNumber.addEventListener('keyup', getEnterNumber);
+//Attemps counter
+function setAttemps() {
+  attemps = attemps + 1;
+  attempsBox.innerHTML = `Número de intentos: ${attemps}`;
+}
+
+function handlerCalculation() {
+  compareNumbers();
+  setAttemps();
+}
+
+//Listeners
 tryButton.addEventListener('click', handlerCalculation);
